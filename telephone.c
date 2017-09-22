344eac35b3c8a0e85bb787cdd163d30055fd866e
Peter <pl488@cornell.edu>
2017-09-22 19:35:00 -0400

    print statements

--- telephone.c
+++ telephone.c
@@ -57,11 +57,12 @@ int main(int argc, char **argv)
     // Send and Receive Loop
     int i;
     int send_rank = (world_rank + 1) % world_size;
-    int recv_rank = (world_rank - 1) < 0 ? world_size : (world_rank - 1);
+    int recv_rank = (world_rank - 1) < 0 ? (world_size - 1) : (world_rank - 1);
     for (i = 0; i < world_size; i++) {
         if (world_rank == i) {
 
             MPI_Send(&buf, 1, MPI_BYTE, send_rank, 0, MPI_COMM_WORLD);
+            printf('inside %d sending to %d', world_rank, send_rank);
 
             /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             ~~~~~~~~~~~~~~Complete: ~~~~~~~~~~~~~~~~
@@ -73,8 +74,8 @@ int main(int argc, char **argv)
 
         } else if (world_rank == (i+1) % world_size) {
 
-            int partner_rank =
             MPI_Recv(&buf, 1, MPI_BYTE, recv_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf('inside %d receiving from %d', world_rank, recv_rank);
 
             /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             ~~~~~~~~~~~~~~Complete: ~~~~~~~~~~~~~~~~
