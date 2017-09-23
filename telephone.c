824505f4179125408ff3d0823596935078c395c5
Peter <pl488@cornell.edu>
2017-09-22 21:04:41 -0400

    boundaries fix

--- telephone.c
+++ telephone.c
@@ -72,7 +72,7 @@ int main(int argc, char **argv)
 
         } else if (world_rank == (i+1) % world_size) {
 
-            MPI_Recv(&buf, 1, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&buf, 1, MPI_CHAR, (i - 1) % world_size, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             printf("inside %d receiving %s from %d\n", i, buf, i);
 
             /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
